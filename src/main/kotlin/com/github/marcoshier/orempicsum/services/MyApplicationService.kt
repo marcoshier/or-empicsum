1b37f4ac6b2666ad1503a2344914e39d1de425e3
GitHub Action <action@github.com>
2022-12-26 21:25:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/marcoshier/orempicsum/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/marcoshier/orempicsum/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcoshier.orempicsum.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcoshier.orempicsum.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/marcoshier/orempicsum/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marcoshier/orempicsum/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcoshier.orempicsum.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcoshier.orempicsum.MyBundle
 
 class MyProjectService(project: Project) {
 
